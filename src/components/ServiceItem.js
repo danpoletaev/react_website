8510e316250354585a813fd7e4c451b6f16ff3aa
Daniil Poletaev <danpoletaev1@gmail.com>
2020-10-12 10:16:51 +0200

    Formatted ServiceItem Component
    -Changed hard-coded pieces with information from props

--- src/components/ServiceItem.js
+++ src/components/ServiceItem.js
@@ -1,36 +1,42 @@
 import React from 'react'
 import {makeStyles} from "@material-ui/core/styles";
-
-import stages_main from "../images/icon/ic_calc.png"
-import workforce_img from "../images/WorkforceM/background_2.png";
+import {Container, Col, Row, Navbar, Nav, Image} from "react-bootstrap";
+import './styles/serviceItem.scss'
 
 const useStyles = makeStyles({
     container: {
-        width: '500px',
-        height: '400px',
         borderRadius: '25px',
         padding: '50px',
-        marginLeft: '50px',
         boxShadow: '0px 0px 21px -1px rgba(136,136,136,1)',
+        height: '415px',
+        width: '530px'
     }
 })
 
-const ServiceItem = () => {
+const ServiceItem = (props) => {
 
     const classes = useStyles()
 
     return (
-        <div className={classes.container}>
-            <img style={{height: 'auto', width: '20%', marginBotom: '10px'}} src={stages_main}/>
-            <h4>Нормирование и калькулятор численности</h4>
-            <br/>
-            <ul>
-                <li>Анализ бизнес-процессов</li>
-                <li>Нормирование бизнес-процессов</li>
-                <li>Описание бизнес-процессов и рекомендации по оптимизации</li>
-                <li>Разработка модели расчет численности</li>
-            </ul>
-        </div>
+        <Container className={classes.container} style={{backgroundImage: `url(${props.back})`}}>
+            <Row>
+                <Col md={12}>
+                    <img style={{height: 'auto', width: '20%'}} src={props.icon}/>
+                </Col>
+                <Col md={12}>
+                    <h4 style={{marginTop: '10px'}}>{props.header}</h4>
+                </Col>
+                <br/>
+                <Col>
+                    <ul className='unorderedList'>
+                        {props.bulletList.map(bullet => <li>{bullet}</li>)}
+                    </ul>
+                </Col>
+            </Row>
+            {/*<Row className='d-flex flex-row-reverse'>*/}
+            {/*    <img style={{height: 'auto', width: '20%'}} className='align-bottom' src={props.back}/>*/}
+            {/*</Row>*/}
+        </Container>
     )
 }
 
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,22 +1,54 @@
 import React from 'react'
 import ServiceItem from "./ServiceItem";
 import {Container, Col, Row, Navbar, Nav, Image} from "react-bootstrap";
+import {makeStyles} from "@material-ui/core/styles";
 
+//import icons
+import ic_calculator from "../images/icon/ic_calc.png"
+import ic_graph from "../images/icon/ic_graph.png"
+import back1 from '../images/icon/back1.png'
+import back2 from '../images/icon/back2.png'
+import back3 from '../images/icon/back3.png'
+import back4 from '../images/icon/back4.png'
+import back5 from '../images/icon/back5.png'
+
+const useStyles = makeStyles({
+    serviceHeader: {
+        padding: '10px',
+        marginTop: '7em'
+    }
+})
 
 const Services = () => {
+
+    const classes = useStyles()
 
     return (
         <>
             <Row>
-                <Col>
-                    <h3>Услуги</h3>
+                <Col md={12} className={classes.serviceHeader}>
+                    <h1 className='text-center'>Услуги</h1>
                 </Col>
             </Row>
-            <Row>
-                <Col>
-                    <ServiceItem/>
+            <Row className='justify-content-center'>
+                <Col md={5}>
+                    <ServiceItem
+                        header='Нормирование и калькулятор численности'
+                        bulletList={['Анализ бизнес-процессов', 'Нормирование бизнес-процессов', 'Описание бизнес-процессов и рекомендации по оптимизации', 'Разработка модели расчет численности']}
+                        icon={ic_calculator}
+                        back={back1}
+                    />
+                </Col>
+                <Col md={5}>
+                    <ServiceItem
+                        header='Планирование гибких графиков'
+                        bulletList={['Прогназирование бизнес-драйверов', 'Методология построения рабочей нагрузки на уровен часа', 'Планирование графиков работы для внутреннего персонала', 'Управление внешним персоналом']}
+                        icon={ic_graph}
+                        back={back2}
+                    />
                 </Col>
             </Row>
+
         </>
     )
 }
